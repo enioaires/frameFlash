b56e8b747c8fad8da72e33695538be668a98030f
enioaires <enio.aires@hotmail.com>
2023-11-07 10:01:53 -0300

    Ajustando schema

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,12 +4,12 @@ export const SignupSchema = z.object({
   username: z.string().min(2, { message: 'Username precisa de pelo menos 2 caracteres' }).max(50),
   name: z.string().min(2, { message: 'Nome precisa de pelo menos 2 caracteres' }).max(50),
   email: z.string().email({ message: 'Email inválido' }),
-  password: z.string().min(8, { message: 'Senha precisa ter pelo menos 6 caracteres' }).max(50),
+  password: z.string().min(8, { message: 'Senha precisa ter pelo menos 8 caracteres' }).max(50),
 });
 
 export const SigninSchema = z.object({
   email: z.string().email({ message: 'Email inválido' }),
-  password: z.string().min(8, { message: 'Senha precisa ter pelo menos 6 caracteres' }).max(50),
+  password: z.string().min(8, { message: 'Senha precisa ter pelo menos 8 caracteres' }).max(50),
 });
 
 export const PostSchema = z.object({
