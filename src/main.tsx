1c7f35958e1b63af7ce19b59a8cfbdc331e11d2b
Enio <enio@mail.com>
2025-06-04 09:52:13 -0300

    analytics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +14,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </AuthProvider>
     </QueryClientProvider>
   </BrowserRouter>
